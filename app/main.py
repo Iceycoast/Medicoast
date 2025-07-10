443395af6e236b3e68b96d5895bc86e0ff683066
Iceycoast <amitbalab9@gmail.com>
2025-07-10 14:19:26 +0530

    added router for sleep tracker

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.users.routes import router as user_router
 from app.trackers.water_tracker.routes import router as water_router
 from app.trackers.meals_tracker.routes import router as meal_router
 from app.trackers.bmi_tracker.routes import router as bmi_router
+from app.trackers.sleep_tracker.routes import router as sleep_router
 from app.startup import load_all_schemas
 
 @asynccontextmanager
@@ -18,3 +19,4 @@ app.include_router(user_router, prefix="/users", tags=["Users"])
 app.include_router(water_router)
 app.include_router(meal_router)
 app.include_router(bmi_router)
+app.include_router(sleep_router)
