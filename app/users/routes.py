1590361b51469efda8a70c68de105ef6d0146819
Iceycoast <amitbalab9@gmail.com>
2025-07-18 20:27:39 +0530

    added jwt authentication to this

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,17 +1,19 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 from app.users.models import UserCreate, UserLogin, UserResponse
 from . import controller
+from app.auth.jwt import create_access_token
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/register", response_model= UserResponse, status_code= status.HTTP_201_CREATED)
-def register(user:UserCreate):
+@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def register(user: UserCreate):
     return controller.register_user(user)
 
-@router.post("/login", response_model= UserResponse)
+@router.post("/login")
 def login(data: UserLogin):
-    return controller.login_user(data)
-
+    user = controller.login_user(data)
+    access_token = create_access_token({"sub": user["user_id"]})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @router.get("/check-username/{username}")
 def check_username(username: str):
