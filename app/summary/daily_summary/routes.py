7fd33411cc2582a70a6c4b3d50878938703a681d
Iceycoast <amitbalab9@gmail.com>
2025-07-18 20:52:12 +0530

    fixed route

--- app/summary/daily_summary/routes.py
+++ app/summary/daily_summary/routes.py
@@ -5,7 +5,7 @@ from app.auth.dependencies import get_current_user
 
 router = APIRouter(prefix="/summary", tags=["Summary"])
 
-@router.get("/daily")
+@router.get("/daily", response_model= DailySummary)
 def daily_summary(date: str = Query(...), current_user: dict = Depends(get_current_user)):
     user_id = current_user["user_id"]
     return get_daily_logs(user_id, date)
